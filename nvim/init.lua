90c4378b15b443e385464a74607b2dd9cacb38c0
Dan Bruce <dan@shipd.dev>
2025-01-03 14:00:51 +0000

    Copilot Key Configuration
    - Removes the copilot extra in favour of copilot.lua which allows selecting words
      and lines as well as accepting all.
    - Installs Oil.nvim although I'm not sure I'll use it that much or at all.
    - Sets up Ghostty to enable alt as the meta key.
    - Adds hbs file as html association in Neovim.

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,5 +2,5 @@
 require("config.lazy")
 
 -- Associated ejs files with html.
-vim.filetype.add({ extension = { ejs = "html" } })
+vim.filetype.add({ extension = { ejs = "html", hbs = "html" } })
 -- vim.cmd("colorscheme cyberdream")
